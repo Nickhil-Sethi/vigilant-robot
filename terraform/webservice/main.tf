0b7069197e3942590e2f007055d3d599b628e0a7
Nickhil Sethi <ns1635@nyu.edu>
2020-10-10 19:11:04 -0400

    script working

--- terraform/webservice/main.tf
+++ terraform/webservice/main.tf
@@ -1,5 +1,11 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-west-2"
+}
+
+variable "keypair_name" {
+  description = "the name of the ssh key used to access the ec2 instances"
+  type        = string
+  default     = "nickhil_sethi_ec2_ssh"
 }
 
 resource "aws_security_group" "instance" {
@@ -18,17 +24,27 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "example" {
-  ami                    = "ami-0c55b159cbfafe1f0"
-  instance_type          = "t2.micro"
+  ami                    = "ami-0d5d1a3aa3516231f"
+  instance_type          = "t2.large"
   vpc_security_group_ids = [aws_security_group.instance.id]
-
-  user_data = <<-EOF
+  key_name               = "${var.keypair_name}"
+  user_data              = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              sudo yum install -y git golang
+              export HOME=/home/ec2-user
+              cd /home/ec2-user; git clone https://github.com/Nickhil-Sethi/vigilant-robot.git
+              cd vigilant-robot; go build -o hello ./hello-world
+              ./hello &
               EOF
 
   tags = {
