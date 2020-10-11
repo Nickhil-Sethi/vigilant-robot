f6818403c72b9417867f24bdab9c9438678a780a
Nickhil Sethi <ns1635@nyu.edu>
2020-10-10 20:09:50 -0400

    more of module fleshed out

--- terraform/loadbalanced-service/main.tf
+++ terraform/loadbalanced-service/main.tf
@@ -8,12 +8,18 @@ variable "keypair_name" {
   default     = "nickhil_sethi_ec2_ssh"
 }
 
+variable "ingress_port" {
+  description = "port at which webservice can be accessed"
+  type        = number
+  default     = 80
+}
+
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 0
+    to_port     = var.ingress_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -34,25 +40,34 @@ resource "aws_security_group" "instance" {
 }
 
 resource "aws_launch_configuration" "example" {
-  ami                    = "ami-0d5d1a3aa3516231f"
+  image_id               = "ami-0d5d1a3aa3516231f"
   instance_type          = "t2.large"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
   key_name               = "${var.keypair_name}"
   user_data              = <<-EOF
               #!/bin/bash
               sudo yum install -y git golang
+              iptables -A PREROUTING -t nat -p tcp --dport ${var.ingress_port} -j REDIRECT --to-ports 8080
               export HOME=/home/ec2-user
               cd /home/ec2-user; git clone https://github.com/Nickhil-Sethi/vigilant-robot.git
               cd vigilant-robot; go build -o hello ./hello-world
               ./hello &
               EOF
 
-  tags = {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-output "public_ip" {
-  value = aws_instance.example.public_ip
-  description = "the public ip address of the deployed ec2 instance"
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
 }
